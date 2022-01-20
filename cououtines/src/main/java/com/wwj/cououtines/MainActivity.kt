3220c571c9999b7899a52ef2a45224aa04a1a159
wen <githubwwj@github.com>
2022-01-20 18:55:57 +0800

    1 新增协程学习项目

---
+++ cououtines/src/main/java/com/wwj/cououtines/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.wwj.cououtines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.wwj.cououtines.viewmodel.MainVM
+import kotlin.coroutines.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val mainVM: MainVM by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myCoroutine = suspend {
+            5
+        }.createCoroutine(object : Continuation<Int> {
+            override val context: CoroutineContext
+                get() = EmptyCoroutineContext
+
+            override fun resumeWith(result: Result<Int>) {
+                Log.d("tag", "conoutines end")
+            }
+        })
+
+        myCoroutine.resume(Unit)
+
+
+    }
+}
