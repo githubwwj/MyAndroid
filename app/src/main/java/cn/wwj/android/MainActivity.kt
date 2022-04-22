10a1cf4397992c2e80461c0f45cce9ef7455dd89
wwj <wenjian@lkhaowu.onaliyun.com>
2022-04-22 17:32:52 +0800

    1 删除不用的类

--- app/src/main/java/cn/wwj/android/MainActivity.kt
+++ app/src/main/java/cn/wwj/android/MainActivity.kt
@@ -1,26 +1,15 @@
 package cn.wwj.android
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
-import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
-import android.view.Window
-import android.view.WindowManager
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import com.bumptech.glide.RequestBuilder
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.request.target.Target
-import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
+
     var which = 0
-    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -59,7 +48,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        unbindService(serviceConnection)
     }
 
 }
